9102e6598175b390b332c41f24d931c3c846a3fa
mjjang <inseok16@gmail.com>
2020-03-08 09:12:05 +0900

    fix error

--- ApartmentSns/app/src/main/java/com/mjjang/apartmentsns/data/Post.kt
+++ ApartmentSns/app/src/main/java/com/mjjang/apartmentsns/data/Post.kt
@@ -1,21 +1,24 @@
 package com.mjjang.apartmentsns.data
 
+import androidx.annotation.Nullable
 import androidx.room.*
 import java.util.*
 
 @Entity(
     tableName = "posts",
-    foreignKeys = [
-        ForeignKey(entity = Apartment::class, parentColumns = ["id"], childColumns = ["apartment_id"])
-    ],
-    indices = [Index("apartment_id")]
+    foreignKeys = arrayOf(
+        ForeignKey(
+            entity = Apartment::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("apartment_id")
+        )),
+    indices = arrayOf(Index(value = ["apartment_id"]))
 )
 data class Post (
     val apartment_id: Int,
     @PrimaryKey val post_id: Int,
     val title: String,
     val writer: String,
-    val create_date: Calendar = Calendar.getInstance(),
     val content: String,
     val src_url: String
 ){
