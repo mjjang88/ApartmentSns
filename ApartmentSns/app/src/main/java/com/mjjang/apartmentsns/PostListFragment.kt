1650d352cee9e5bbce08e468a92129850e8b7d2d
mjjang <inseok16@gmail.com>
2020-03-30 08:25:01 +0900

    포스트 추가할 수 있는 플로팅 버튼 추가

--- ApartmentSns/app/src/main/java/com/mjjang/apartmentsns/PostListFragment.kt
+++ ApartmentSns/app/src/main/java/com/mjjang/apartmentsns/PostListFragment.kt
@@ -39,6 +39,8 @@ class PostListFragment : Fragment() {
         subscribeUi(adapter)
 
         setHasOptionsMenu(true)
+
+        binding.floatingBtnAddPost.setOnClickListener {  }
         return binding.root
     }
 
